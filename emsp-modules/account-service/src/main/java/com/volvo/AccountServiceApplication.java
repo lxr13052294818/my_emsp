90fd2daf7fb00f443d7a18e737423909c510706c
lxr <LiuXingrui>
2025-02-24 16:28:26 +0800

    integrate Mapper

--- emsp-modules/account-service/src/main/java/com/volvo/AccountServiceApplication.java
+++ emsp-modules/account-service/src/main/java/com/volvo/AccountServiceApplication.java
@@ -1,5 +1,6 @@
 package com.volvo;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -14,6 +15,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@MapperScan("com.volvo.**.mapper")
 public class AccountServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccountServiceApplication.class, args);
