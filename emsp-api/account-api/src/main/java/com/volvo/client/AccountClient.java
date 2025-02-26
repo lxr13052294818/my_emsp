cbf7540c04516c76764110a38cc02e64c8cb8762
lxr <LiuXingrui>
2025-02-26 21:10:32 +0800

    develop openfeign initially

---
+++ emsp-api/account-api/src/main/java/com/volvo/client/AccountClient.java
@@ -0,0 +1,10 @@
+package com.volvo.client;
+
+/**
+ * 描述:
+ *
+ * @author lxr
+ * @date 2025/2/26
+ */
+public class AccountClient {
+}
--- emsp-modules/card-service/src/main/java/com/volvo/CardServiceApplication.java
+++ emsp-modules/card-service/src/main/java/com/volvo/CardServiceApplication.java
@@ -4,7 +4,7 @@ package com.volvo;
  * Hello world!
  *
  */
-public class App 
+public class CardServiceApplication
 {
     public static void main( String[] args )
     {
