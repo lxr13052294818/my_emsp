a50658a2a3b3b4d75f3269facb78a0f119932837
lxr <LiuXingrui>
2025-03-01 12:24:25 +0800

    mybatis plus pagination

---
+++ emsp-common/emsp-common-config/src/main/java/com/volvo/MyBatisPlusConfig.java
@@ -0,0 +1,23 @@
+package com.volvo;
+
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 描述: mybatis plus 分页器
+ *
+ * @author lxr
+ * @date 2025/3/1
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    /**
+     * 创建并返回分页插件实例
+     *
+     * @return PaginationInterceptor
+     */
+//    @Bean
+//    public PaginationInterceptor paginationInterceptor() {
+//        return new PaginationInterceptor();
+//    }
+}
